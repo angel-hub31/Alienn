09ea32098bfbd41f470df904e12f021f71ec25c7
angel-hub31 <moralesmales1@hotmail.com>
2026-04-17 19:52:09 -0500

    se crea los atributos privados

---
+++ src/com/krakedev/alien/Alien.java
@@ -0,0 +1,14 @@
+package com.krakedev.alien;
+
+public class Alien {
+	private int tamanio;
+    private String color;
+    private int numeroOjos;
+    private int numeroBrazos;
+    private int numeroPies;
+    private double precioExtremidad;
+    private double precioOjo;
+    private double precioCuerpo;
+    private double precioTotal;
+
+}
