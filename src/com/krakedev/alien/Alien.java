db8baf5218fc9a3a0de8cd4380008ee3775364c9
angel-hub31 <moralesmales1@hotmail.com>
2026-04-17 19:57:44 -0500

    se crea los get y el metodo imprimir

--- src/com/krakedev/alien/Alien.java
+++ src/com/krakedev/alien/Alien.java
@@ -2,39 +2,83 @@ package com.krakedev.alien;
 
 public class Alien {
 	private int tamanio;
-    private String color;
-    private int numeroOjos;
-    private int numeroBrazos;
-    private int numeroPies;
-    private double precioExtremidad;
-    private double precioOjo;
-    private double precioCuerpo;
-    private double precioTotal;
-    
-    
-    public Alien(int tamanio, String color) {
+	private String color;
+	private int numeroOjos;
+	private int numeroBrazos;
+	private int numeroPies;
+	private double precioExtremidad;
+	private double precioOjo;
+	private double precioCuerpo;
+	private double precioTotal;
 
-        if (tamanio < 5) {
-            this.tamanio = 5;
-        } else if (tamanio > 30) {
-            this.tamanio = 30;
-        } else {
-            this.tamanio = tamanio;
-        }
+	public Alien(int tamanio, String color) {
 
-        this.color = color;
+		if (tamanio < 5) {
+			this.tamanio = 5;
+		} else if (tamanio > 30) {
+			this.tamanio = 30;
+		} else {
+			this.tamanio = tamanio;
+		}
 
-        this.numeroOjos = 0;
-        this.numeroBrazos = 0;
-        this.numeroPies = 0;
+		this.color = color;
 
-        this.precioCuerpo = this.tamanio * 0.20;
-        this.precioExtremidad = this.tamanio * 0.10;
-        this.precioOjo = this.tamanio * 0.05;
+		this.numeroOjos = 0;
+		this.numeroBrazos = 0;
+		this.numeroPies = 0;
 
-        this.precioTotal = 0;
+		this.precioCuerpo = this.tamanio * 0.20;
+		this.precioExtremidad = this.tamanio * 0.10;
+		this.precioOjo = this.tamanio * 0.05;
 
-       // calcularPrecioTotal();
-    }
+		this.precioTotal = 0;
+
+		// calcularPrecioTotal();
+	}
+
+	public int getTamanio() {
+		return tamanio;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public int getNumeroOjos() {
+		return numeroOjos;
+	}
+
+	public int getNumeroBrazos() {
+		return numeroBrazos;
+	}
+
+	public int getNumeroPies() {
+		return numeroPies;
+	}
+
+	public double getPrecioExtremidad() {
+		return precioExtremidad;
+	}
+
+	public double getPrecioOjo() {
+		return precioOjo;
+	}
+
+	public double getPrecioCuerpo() {
+		return precioCuerpo;
+	}
+
+	public double getPrecioTotal() {
+		return precioTotal;
+	}
+
+	public void imprimir() {
+		System.out.println("Tamaño: " + tamanio);
+		System.out.println("Color: " + color);
+		System.out.println("Brazos: " + numeroBrazos);
+		System.out.println("Piernas: " + numeroPies);
+		System.out.println("Ojos: " + numeroOjos);
+		System.out.println("Precio total: " + precioTotal);
+	}
 
 }
